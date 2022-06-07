54978e23617b330a9766266790d56bb01b64bbae
Christian Alcantara <chriscayana@gmail.com>
2022-06-07 09:20:35 -0700

    fix

---
+++ seed-file/categorySeed.js
@@ -0,0 +1,30 @@
+[{
+  name: "Team Fight Tactics",
+  voteCount: "15",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: "League of Legends",
+  voteCount: "10",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: "Apex Legends",
+  voteCount: "30",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: "Valorant",
+  voteCount: "20",
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name: "Hollow Knight",
+  voteCount: "300",
+  createdAt: new Date(),
+  updatedAt: new Date()
+}]
