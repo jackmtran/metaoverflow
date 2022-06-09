a2ed170cd684279f21e90794536d6c28cacb0cc6
Christian Alcantara <chriscayana@gmail.com>
2022-06-09 10:50:43 -0700

    delete button on page

---
+++ public/javascripts/questionDelete.js
@@ -0,0 +1,16 @@
+const deleteBtns = document.querySelectorAll('.delete-btn')
+
+for (let i = 0; i < deleteBtns.length; i++) {
+    const btn = deleteBtns[i];
+
+    btn.addEventListener('click', async(e) => {
+        const questionId = e.target.id.split('-')[2]
+        console.log(questionId);
+        const res = await fetch(`/questions/${questionId}`, {
+            method: 'DELETE'
+        })
+        if (res) {
+            window.location.href=('/questions')
+        }
+    })
+}
